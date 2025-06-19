6956ae325744845f6a09e3e07fa1b9c5dfcd5a24
Adesh <adesh02092000@gmail.com>
2025-06-20 02:13:41 +0530

    Setup basic sidebar

--- apps/web/app/(main)/layout.tsx
+++ apps/web/app/(main)/layout.tsx
@@ -1,5 +1,11 @@
+import { AppSidebar } from "@/components/app-sidebar"
 import MaxWidthWrapper from "@/components/max-width-wrapper"
 import Navbar from "@/components/navbar"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@workspace/ui/components/sidebar"
 import { NuqsAdapter } from "nuqs/adapters/next/app"
 
 export default function MainLayout({
@@ -9,10 +15,18 @@ export default function MainLayout({
 }) {
   return (
     <>
-      <NuqsAdapter>
-        <Navbar />
-        <MaxWidthWrapper>{children}</MaxWidthWrapper>
-      </NuqsAdapter>
+      <SidebarProvider className="flex h-screen w-screen flex-col">
+        <AppSidebar />
+        <NuqsAdapter>
+          <Navbar />
+          <MaxWidthWrapper>
+            <SidebarInset>
+              <SidebarTrigger className="md:-ml-12" />
+            </SidebarInset>
+            {children}
+          </MaxWidthWrapper>
+        </NuqsAdapter>
+      </SidebarProvider>
     </>
   )
 }
