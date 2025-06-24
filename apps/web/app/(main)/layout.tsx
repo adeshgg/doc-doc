a3cc48cf115f5628e25689ae96c61cae1e6133bb
Adesh <adesh02092000@gmail.com>
2025-06-24 18:30:43 +0530

    Swapped nav shadow from separator

--- apps/web/app/(main)/layout.tsx
+++ apps/web/app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import MaxWidthWrapper from "@/components/max-width-wrapper"
 import Navbar from "@/components/navbar"
+import { Separator } from "@workspace/ui/components/separator"
 import { NuqsAdapter } from "nuqs/adapters/next/app"
 
 export default function MainLayout({
@@ -10,6 +11,7 @@ export default function MainLayout({
   return (
     <NuqsAdapter>
       <Navbar />
+      <Separator />
       <MaxWidthWrapper>{children}</MaxWidthWrapper>
     </NuqsAdapter>
   )
