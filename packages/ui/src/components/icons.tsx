e2fa08c4ae44ceec47e97a8f6d2e6b906f00ad9f
Adesh <adesh02092000@gmail.com>
2025-06-30 17:41:37 +0530

    refactor(navbar): Updated layout and added more links

--- packages/ui/src/components/icons.tsx
+++ packages/ui/src/components/icons.tsx
@@ -53,4 +53,31 @@ export const Icons = {
       <path d="M9 18c-4.51 2-5-2-7-2" />
     </svg>
   ),
+  hamburger: (props: React.SVGProps<SVGSVGElement>) => (
+    <svg
+      className="pointer-events-none"
+      width={16}
+      height={16}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4 12L20 12"
+        className="origin-center -translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[315deg]"
+      />
+      <path
+        d="M4 12H20"
+        className="origin-center transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.8)] group-aria-expanded:rotate-45"
+      />
+      <path
+        d="M4 12H20"
+        className="origin-center translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[135deg]"
+      />
+    </svg>
+  ),
 }
