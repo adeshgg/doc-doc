07daf4c6d66bcdc1d7aa056fb89550c7bea08892
Adesh <adesh02092000@gmail.com>
2025-07-02 17:47:53 +0530

    (feat: metadata): Add metadata

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -4,6 +4,8 @@ import "@workspace/ui/globals.css"
 import { Providers } from "@/components/providers"
 import { TRPCReactProvider } from "@/trpc/react"
 import { Toaster } from "sonner"
+import { siteConfig } from "@/lib/const"
+import { Metadata } from "next"
 
 const fontSans = Geist({
   subsets: ["latin"],
@@ -20,6 +22,62 @@ const spaceGrotesk = Space_Grotesk({
 
   subsets: ["latin"],
 })
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s • ${siteConfig.name}`,
+  },
+  metadataBase: new URL(siteConfig.url),
+  description: siteConfig.description,
+  keywords: siteConfig.keywords,
+  authors: [
+    {
+      name: siteConfig.author.name,
+      url: siteConfig.author.url,
+    },
+  ],
+  creator: siteConfig.author.name,
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@adeshgg",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+}
 
 export default function RootLayout({
   children,
--- apps/web/lib/const.ts
+++ apps/web/lib/const.ts
@@ -1,1 +1,42 @@
 export const MAX_FILE_SIZE = 4
+
+export const siteConfig = {
+  name: "doc-doc.",
+  url: `${process.env.NEXT_PUBLIC_APP_URL}`,
+  ogImage: `${process.env.NEXT_PUBLIC_APP_URL}/og.png`,
+  description: "Doc Doc • Organize Your Medical Records",
+  links: {
+    twitter: "https://x.com/adeshgg",
+    github: "https://github.com/adeshgg",
+  },
+  author: {
+    name: "Adesh Gupta",
+    url: `https://x.com/adeshgg`,
+  },
+  keywords: [
+    "Medical Document AI",
+    "AI Chat",
+    "Medical Records Chatbot",
+    "Health Document Assistant",
+    "Personal Health AI",
+    "Medical Information Retrieval",
+    "Document Q&A AI",
+    "Healthcare AI Assistant",
+    "RAG AI",
+    "Retrieval Augmented Generation",
+    "Large Language Model (LLM)",
+    "Natural Language Processing (NLP)",
+    "AI-powered Medical",
+    "Self-hosted Medical AI",
+    "Community-driven Medical AI",
+    "Medical Insights",
+    "Health Information Management",
+    "Personalized Health Advice",
+    "Understand Medical Reports",
+    "Medical Document Organization",
+    "Healthcare Document Analysis",
+    "Easy Medical Q&A",
+  ],
+}
+
+export type SiteConfig = typeof siteConfig
